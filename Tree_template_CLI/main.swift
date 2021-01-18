e31616be72f38fdbaab032a6a95a7055f6cf0af2
Testing_author <testing_beta@mail.com>
2021-01-18 16:15:40 +0200

    Building the basis of the tree, creating nodes and connecting them.

--- Tree_template_CLI/main.swift
+++ Tree_template_CLI/main.swift
@@ -6,6 +6,47 @@
 //
 
 import Foundation
-
 print("Hello, World!")
 
+
+struct Node<Value> {
+    var value: Value
+   private (set) var children: [Node]
+    
+    
+    mutating func add(child: Node) {
+        children.append(child)
+    }
+    
+    
+    init(_ value: Value) {
+        self.value = value
+        children = [ ]
+    }
+    
+    
+    init(_ value: Value, children: [Node]) {
+        self.value = value
+        self.children = children
+    }
+}
+
+
+//creating nodes and model them
+
+var andrei = Node("Andrei")
+var alexei = Node("Alexei")
+andrei.add(child: alexei)
+
+var bianca = Node("Bianca")
+var bogdan = Node("Bogdan")
+var bucur = Node("Bucur")
+bianca.add(child: bogdan)
+bianca.add(child: bucur)
+
+var root = Node("Cristi")
+root.add(child: andrei)
+root.add(child: bianca)
+
+print(root)
+print(andrei)
